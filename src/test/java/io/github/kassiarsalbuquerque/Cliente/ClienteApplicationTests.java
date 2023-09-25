7c76316813c70ee4624151019c8c997ff981e02f
Kassia <kassia.r.albuquerque@accenture.com>
2023-09-25 08:17:40 -0300

    Estrutura

---
+++ src/test/java/io/github/kassiarsalbuquerque/Cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kassiarsalbuquerque.Cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
